f3b9d00360bfe89837502b9e3c5e93e4b85c3527
dhafin-k <dhafinekresna@gmail.com>
2024-11-27 06:40:20 +0700

    Main

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+// import React from 'react'
+
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skill from "./components/Skill";
+import Work from "./components/Work";
+import Review from "./components/Review";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <>
+    <Header />
+    <main>
+      <Hero />
+      <About />
+      <Skill />
+      <Work />
+      <Review />
+      <Contact />
+    </main>
+    </>
+  )
+}
+
+export default App
