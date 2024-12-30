0c1af60563b79a974803b31d1d57c5d55b2a7544
dhafin-k <dhafinekresna@gmail.com>
2024-12-30 21:13:37 +0700

    update#2

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -10,7 +10,7 @@ const SkillCard = ({
 }) => {
   return (
     <div className={'flex items-center gap-3 ring-2 ring-inset ring-zinc-50/10 rounded-2xl p-3 hover:bg-zinc-800 transition-colors group ' + classes}>
-        <figure className="bg-zinc-700/50 rounded-lg overflow-hidden w-12 h-12 p-2 group-hover:bg-zinc-900 transition-colors"> 
+        <figure className="w-12 h-12 p-2 overflow-hidden transition-colors rounded-lg bg-zinc-700/50 group-hover:bg-zinc-900"> 
             <img 
             src={imgSrc}
             width={32}
@@ -22,7 +22,7 @@ const SkillCard = ({
         <div>
             <h3>{label}</h3>
 
-            <p className="text-zinc-400 text-sm">
+            <p className="text-sm text-zinc-400">
                 {desc}
             </p>
         </div>
